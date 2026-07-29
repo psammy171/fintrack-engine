ecf0b313a6309a7674c33116c6233e8386c08163
Sammy P <psammy@Sammys-iMac.local>
2026-07-29 21:06:14 +0530

    add delete expense api

--- src/main/java/com/sammedsp/fintrack/repositories/ExpenseShareRepository.java
+++ src/main/java/com/sammedsp/fintrack/repositories/ExpenseShareRepository.java
@@ -1,7 +1,13 @@
 package com.sammedsp.fintrack.repositories;
 
 import com.sammedsp.fintrack.entities.ExpenseShare;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExpenseShareRepository extends JpaRepository<ExpenseShare, String> {
+    List<ExpenseShare> findAllByRootTransactionId(String transactionId);
+
+    void deleteByRootTransactionId(String transactionId);
 }
